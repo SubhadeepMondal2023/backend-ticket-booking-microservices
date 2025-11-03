35173baac48a129735dfe7c1fa1759dcecae64f4
Subha <msubhadeep9635@gmail.com>
2025-11-04 01:51:36 +0530

    feat: enhance booking service with Kafka integration; add BookingEvent class, update BookingService to send booking events, and configure Kafka properties

--- bookingservice/src/main/java/com/ticketbooking/bookingservice/service/BookingService.java
+++ bookingservice/src/main/java/com/ticketbooking/bookingservice/service/BookingService.java
@@ -1,25 +1,36 @@
 package com.ticketbooking.bookingservice.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.ticketbooking.bookingservice.client.InventoryServiceClient;
 import com.ticketbooking.bookingservice.entity.Customer;
+import com.ticketbooking.bookingservice.event.BookingEvent;
 import com.ticketbooking.bookingservice.repository.CustomerRepository;
 import com.ticketbooking.bookingservice.request.BookingRequest;
 import com.ticketbooking.bookingservice.response.BookingResponse;
 import com.ticketbooking.bookingservice.response.InventoryResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class BookingService {
 
     private final CustomerRepository customerRepository;
     private final InventoryServiceClient inventoryServiceClient;
+    private final KafkaTemplate<String, BookingEvent> kafkaTemplate;
 
-    @Autowired
-    public BookingService(final CustomerRepository customerRepository, final InventoryServiceClient inventoryServiceClient){
+     @Autowired
+    public BookingService(final CustomerRepository customerRepository,
+                          final InventoryServiceClient inventoryServiceClient,
+                          final KafkaTemplate<String, BookingEvent> kafkaTemplate) {
         this.customerRepository = customerRepository;
         this.inventoryServiceClient = inventoryServiceClient;
+        this.kafkaTemplate = kafkaTemplate;
     }
     public BookingResponse createBooking(final BookingRequest request){
          // check if user exists
@@ -29,11 +40,34 @@ public class BookingService {
         }
         // check if there is enough inventory
         final InventoryResponse inventoryResponse = inventoryServiceClient.getInventory(request.getEventId());
+        log.info("Inventory Response: {}", inventoryResponse);
         
         if (inventoryResponse.getCapacity() < request.getTicketCount()) {
             throw new RuntimeException("Not enough inventory");
         }
         
-        return BookingResponse.builder().build();
+
+        //create booking
+        final BookingEvent bookingEvent = createBookingEvent(request, customer, inventoryResponse);
+         // send booking to Order Service on a Kafka Topic
+        kafkaTemplate.send("booking", bookingEvent);
+        log.info("Booking sent to Kafka: {}", bookingEvent);
+        return BookingResponse.builder()
+                .userId(bookingEvent.getUserId())
+                .eventId(bookingEvent.getEventId())
+                .ticketCount(bookingEvent.getTicketCount())
+                .totalPrice(bookingEvent.getTotalPrice())
+                .build();
+    }
+
+     private BookingEvent createBookingEvent(final BookingRequest request,
+                                            final Customer customer,
+                                            final InventoryResponse inventoryResponse) {
+        return BookingEvent.builder()
+                .userId(customer.getId())
+                .eventId(request.getEventId())
+                .ticketCount(request.getTicketCount())
+                .totalPrice(inventoryResponse.getTicketPrice().multiply(BigDecimal.valueOf(request.getTicketCount())))
+                .build();
     }
 }
